6a6d9892e9a224411a50d0d1b554cd685635820f
Clemens Lechner <clemens.lechner@gesis.org>
2020-11-18 18:46:20 +0100

    First near-final draft of manuscript w/ figure

--- 04_reporting.R
+++ 04_reporting.R
@@ -132,12 +132,12 @@ deltas_plotter <- function(study) {
         paste("(Cohen's ", italic(d[av]), ")")
       ))
     ) +
-    jtools::theme_apa() +
+   jtools::theme_apa() +
     theme(
       text = element_text(family = "Source Sans Pro"),
       legend.text = element_text(size = 15, face = "bold"),
       legend.position = "top",
-      plot.margin = margin(1.25, 0.5, 0.5, 0.5,
+      plot.margin = margin(0.5, 0.5, 0.5, 0.5,
         unit = "cm"
       ),
       strip.background = element_blank(),
@@ -175,10 +175,12 @@ deltas_plotter <- function(study) {
 deltas_plot <- cowplot::plot_grid(
   deltas_plotter("piaac"),
   deltas_plotter("neps"),
+  nrow = 1,
   labels = c("Three years (PIAAC-L)", "Six years (NEPS)"),
   align = "h",
   hjust = -0.1,
-  label_size = 18
+  label_size = 15,
+  label_fontfamily = "Source Sans Pro"
 
 )
 
@@ -228,7 +230,7 @@ cors_plotter <- function(study) {
       text = element_text(family = "Source Sans Pro"),
       legend.text = element_text(size = 15, face = "bold"),
       legend.position = "top",
-      plot.margin = margin(1.25, 0.5, 0.5, 0.5,
+      plot.margin = margin(0.5, 0.5, 0.5, 0.5,
         unit = "cm"
       ),
       strip.background = element_blank(),
@@ -265,10 +267,12 @@ cors_plotter <- function(study) {
 cors_plot <- cowplot::plot_grid(
   cors_plotter("piaac"),
   cors_plotter("neps"),
+  nrow = 1,
   labels = c("Three years (PIAAC-L)", "Six years (NEPS)"),
   align = "h",
   hjust = -0.1,
-  label_size = 18
+  label_size = 15,
+  label_fontfamily = "Source Sans Pro"
 )
 
 #cors_plot
@@ -285,5 +289,150 @@ cors_plot <- cowplot::plot_grid(
 
 # Plot density distribution of change ---------------------------------------------
 # Moved to appendix.Rmd
+
+# Function to get descriptive statistics on socio-demographic characteristics
+
+describe_data <- function(study) {
+  data <- get(study) %>% 
+    filter(.imp == 1)  %>% 
+    mutate(edu0 = if_else(edugr == 0, 1, 0),
+           edu1 = if_else(edugr == 1, 1, 0),
+           edu2 = if_else(edugr == 2, 1, 0),
+           age0 = if_else(agegr == 0, 1, 0),
+           age1 = if_else(agegr == 1, 1, 0),
+           age2 = if_else(agegr == 2, 1, 0),
+           age3 = if_else(agegr == 3, 1, 0),
+           female = if_else(gender == 2, 1, 0),
+           native = if_else(language == 1, 1, 0),
+           # t1_pv = round(t1_pv, 2),
+           # t2_pv = round(t2_pv, 2),
+           across(where(is.double), ~as.numeric(.x))) %>%
+    select(age, age0, age1, age2, age3, female, edu0:edu2 #native, hhsize,
+           #t1_pv, t2_pv
+    ) %>%
+    pivot_longer(cols = everything()) %>%  
+    mutate(target = study,
+           Variables = recode(name,
+                              age = "Age in years",
+                              edu0 = "low (ISCED 0-3)",
+                              edu1 = "intermediate (ISCED 4/5B)",
+                              edu2 = "high (ISCED 5A/6)",
+                              age0 = "18-34 years",
+                              age1 = "35-44 years",
+                              age2 = "45-54 years",
+                              age3 = "55 + years",
+                              hhsize = "N. of person in houseold",
+                              female = "Female",
+                              native = "German native speaker"#,
+                              # t1_pv = "Skill level at T1",
+                              # t2_pv = "Skill level at T2",
+           )) %>% 
+    group_by(Variables) 
+  
+  head(data)
+  
+  descriptives <- data %>%
+    summarize(
+      "Range" = str_c(min(value, na.rm = T), " - ", max(value, na.rm = T)),
+      "\\emph{M}" = mean(value, na.rm = T), 
+      "\\emph{SD}" = sd(value, na.rm = T)
+    )
+  
+  descriptives[-1] <-  printnum(descriptives[-1])
+  
+  descriptives
+  
+}
+
+
+
+# Function to get pooled unweighted descriptive statistics 
+# on skills and skill change
+describe_skills <- function(study) {
+  data <- get(study) %>% 
+    mutate(delta = t2_pv - t1_pv) %>% 
+    group_by(.imp) %>% 
+    summarize(across(c(t1_pv, t2_pv, delta), 
+                     list(Min = min, Max = max, M = mean, SD = sd), 
+                     .names = "{.col}_{.fn}")) %>%
+    summarise(across(everything(), mean)) %>% printnum()
+  
+  domain <- if_else(grepl("reading", study), "Literacy", "Numeracy") 
+  #Construct table manually
+  descriptives <-  rbind(
+    cbind(str_c(domain, " at $T_1$"),
+          str_c(data$t1_pv_Min, " - ", data$t1_pv_Max),
+          data$t1_pv_M,
+          data$t1_pv_SD),
+    cbind(str_c(domain, " at $T_2$"),
+          str_c(data$t2_pv_Min, " - ", data$t2_pv_Max),
+          data$t2_pv_M,
+          data$t2_pv_SD),
+    cbind(str_c("Change in ", domain, " ($\\Delta{T_1, T_2}$)"),
+          str_c(data$delta_Min, " - ", data$delta_Max), 
+          data$delta_M, 
+          data$delta_SD)
+  )
+  
+  descriptives[-1] <-  printnum(descriptives[-1])
+  
+  descriptives
+}
+
+data_descriptives <- cbind(describe_data("reading_piaac"), 
+                           describe_data("reading_neps")[-1])
+
+data_skills <- rbind(cbind(describe_skills("reading_piaac"), 
+                           describe_skills("reading_neps"))[,-5],
+                     cbind(describe_skills("math_piaac"), 
+                           describe_skills("math_neps"))[,-5]) %>% 
+  as.data.frame()
+
+names(data_skills) <- names(data_descriptives)
+
+
+# Additional information --------------------------------------------------
+
+# Compute pooled correlations between change (delta) and initial level (t1)
+cors_pooler2 <- function(target) {
+  data_filtered <- get(target) %>%
+    split(.$.imp) %>%
+    imputationList()
+  
+  sink("NUL")
+  
+  results <- with(data_filtered, lm(scale(t2_pv - t1_pv) ~ scale(t1_pv),
+                                    weights = weight
+  )) %>%
+    MIcombine() %>%
+    summary()
+  
+  sink()
+  
+  resultlist <- tibble(
+    rho = results[["results"]][2],
+    se = results[["se"]][2],
+    lower = results[["(lower"]][2],
+    upper = results[["upper)"]][2]
+  )
+  
+  resultlist
+}
+
+# Create a tibble for computing the PV-based correlations of t1 with delta
+cors_t1_delta <- tibble(
+  target = c("reading_neps", "math_neps", "reading_piaac", "math_piaac"),
+)
+
+# Map the function to the tibble to obtain the results
+cors_t1_delta <- cors_t1_delta %>%
+  rowwise() %>%
+  mutate(ergebnis = list(cors_pooler2(target))) %>%
+  unnest(ergebnis) %>%
+  mutate(
+    domain = str_extract(target, pattern = "reading|math"),
+    study = str_extract(target, pattern = "neps|piaac"), 
+    CI = str_c("[", round(lower,2),"; ", round(upper, 2), "]")
+  )
 
 
